0326ddea3c9036a1b40fbd006c477b29336dc42e
DavidDryden <93728850+DavidDryden@users.noreply.github.com>
2023-05-08 09:55:02 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,17 @@
-function Move_Vertical_with_buttons_at_speed (speed: number, Move: Sprite) {
+function Move_Vertical_with_buttons_at_the_speed_of (speed: number, Move: Sprite) {
     if (controller.up.isPressed() || controller.down.isPressed()) {
         Move.vy = controller.dy(speed)
     } else {
         Move.vy = 0
     }
 }
-function Divide (num1: number, num2: number) {
+function Divide_the_2_numbers (num1: number, num2: number) {
     return num1 / num2
 }
-function Move_Horizontal_with_buttons_at_speed (speed: number, Move: Sprite) {
+function Move_Horizontal_with_buttons_at_the_speed_of (speed: number, Move: Sprite) {
     if (controller.left.isPressed() || controller.right.isPressed()) {
         Move.vx = controller.dx(speed)
     } else {
         Move.vx = 0
     }
 }
-let textSprite = textsprite.create(convertToText(Divide(100, 5)))
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-forever(function () {
-    Move_Horizontal_with_buttons_at_speed(500, textSprite)
-})
